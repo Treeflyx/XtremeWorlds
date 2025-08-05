1f046421648cad168a24c7509ffb4590beacf2fc
Seth <Hellator@gmail.com>
2025-08-04 19:57:03 -0500

    fix: Config

--- Source/Server/Program.cs
+++ Source/Server/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Server.Game;
@@ -6,8 +7,17 @@ using Server.Game.Net;
 using Server.Net;
 using Server.Services;
 
-var builder = Host.CreateApplicationBuilder(args);
+// Get the directory where the executable is located
+var exeDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
+// Create builder with content root set to executable's directory
+var builder = Host.CreateApplicationBuilder(new HostApplicationBuilderSettings
+{
+    ContentRootPath = exeDir,
+    Args = args
+});
+
+// Configure services and logging
 builder.Services.AddSerilog(options => options.ReadFrom.Configuration(builder.Configuration));
 builder.Services.AddHostedService<GameService>();
 builder.Services.AddSingleton<IPlayerService, PlayerService>();
@@ -15,5 +25,4 @@ builder.Services.AddNetworkService<GameSession, GameSessionManager, GameNetworkS
 builder.Services.AddHostedService<ConsoleInputService>();
 
 var app = builder.Build();
-
 await app.RunAsync();
